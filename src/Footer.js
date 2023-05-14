415aeb20b59401e55d3c2a29fa1d3f92f6720201
ReechikaJain <jain.reechika@gmail.com>
2023-05-14 23:38:02 +0530

    Created state

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Footer() {
+    const [data, setdata] = useState('Reechika.....')
+    
   return (
     <div>
-      <h1>Footer</h1>
+      <h1>Bye, {data}</h1>
+      <button onClick={()=>{
+          setdata('Something else .....')
+      }}>
+      </button>
     </div>
   )
 }
